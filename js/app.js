b6fe0a52c67d2a25b5f2459b2167c1d57c72a74a
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-06-20 17:38:56 +0300

    lab

---
+++ js/app.js
@@ -0,0 +1,27 @@
+alert('HELLO IN OUR FOOD WORLD')
+
+let userIn = prompt('Are you prefer meat or chicken?')
+
+if (userIn = "meat") {
+    alert("ymee")
+}
+else if (userIn = "chicken") {
+    alert("me too")
+}
+
+let user = prompt ('wahts your name')
+console.log(user)
+alert('wlcome ' + user + "  to my website"  )
+
+
+
+let userInput = prompt('how many meal do you to eat ' + user)
+console.log(userInput);
+alert("you many welocom " + user)
+
+
+
+let userpre = prompt('Do you like cheat meal ' + user)
+console.log(userpre);
+alert('enjoy')
+
